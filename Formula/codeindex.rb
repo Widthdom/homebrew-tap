6eef85aba0f17a3c5e0ec357e251cf285bb9dd23
Widthdom <125688807+Widthdom@users.noreply.github.com>
2026-08-23 04:26:54 +0000

    Update codeindex to v1.43.1

--- Formula/codeindex.rb
+++ Formula/codeindex.rb
@@ -1,23 +1,23 @@
 class Codeindex < Formula
   desc "CLI code indexing, MCP search, and LSP lookup for local repositories"
   homepage "https://github.com/Widthdom/CodeIndex"
-  version "1.43.0"
+  version "1.43.1"
   license "FSL-1.1-ALv2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Widthdom/CodeIndex/releases/download/v1.43.0/CodeIndex-osx-arm64.tar.gz"
-      sha256 "804a742423b9104f97c5d761144d53fcf36b804eabe1e6f1248ab15bf1503ec3"
+      url "https://github.com/Widthdom/CodeIndex/releases/download/v1.43.1/CodeIndex-osx-arm64.tar.gz"
+      sha256 "3b5afc3f7bb7e8877c186a395059a7454ed00f6b7eee5428e7b2c1023ee9e82e"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Widthdom/CodeIndex/releases/download/v1.43.0/CodeIndex-linux-arm64.tar.gz"
-      sha256 "488a161c70c0f84f9eb043910ffed0e049a47e5d94deb2b74403aef44a036c87"
+      url "https://github.com/Widthdom/CodeIndex/releases/download/v1.43.1/CodeIndex-linux-arm64.tar.gz"
+      sha256 "d2bdd6a9d07cd090dc27d7dab8719c8a6f97bb11e15f5675eb06795d579ed7f1"
     else
-      url "https://github.com/Widthdom/CodeIndex/releases/download/v1.43.0/CodeIndex-linux-x64.tar.gz"
-      sha256 "a37dd6c83ba020114e5ba1151d5cd07e87ab41d03d55206036deeb07197c13ca"
+      url "https://github.com/Widthdom/CodeIndex/releases/download/v1.43.1/CodeIndex-linux-x64.tar.gz"
+      sha256 "d447fdf14f8ccdfefdd267e21afc4e92dff4a0c520afbccf32b8fa25d2b276fd"
     end
   end
 
